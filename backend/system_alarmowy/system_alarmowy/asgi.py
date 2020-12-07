14d5bc0e3e63d49e28d6b641e38966a83dcac226
JJackol <jacekczerwonka@gmail.com>
2020-12-07 22:14:40 +0100

    post merge

--- backend/system_alarmowy/system_alarmowy/asgi.py
+++ backend/system_alarmowy/system_alarmowy/asgi.py
@@ -18,12 +18,12 @@ from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'system_alarmowy.settings')
 
-application = get_asgi_application(),
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter([
-#             re_path(r"^front(end)/$", channel.MeasurementConsumer.as_asgi()),
-#         ])
-#     ),
-# })
+# application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            re_path(r"^front(end)/$", channel.MeasurementConsumer.as_asgi()),
+        ])
+    ),
+})
